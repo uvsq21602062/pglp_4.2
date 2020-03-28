3863a1d77761174222cfd635c4506f828fc1ba68
Jean Destribois <jean.destribois@gmail.com>
2020-03-28 20:44:05 +0100

    jean: Premier commit apres création du projet maven

---
+++ projet-maven/src/main/java/fr/uvsq/uvsq21602062/projet_maven/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21602062.projet_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
